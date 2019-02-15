78f2586f4a800e246bad28a8e2506b9421e3bc3c
Kostya Minkov <hallrizon@gmail.com>
2019-02-15 17:42:04 +0300

    Refactoring office/views and subject/views

--- office/views.py
+++ office/views.py
@@ -14,16 +14,19 @@ class OfficeIndex(LoginRequiredMixin, View):
         user = get_object_or_404(Profile, user=request.user)
 
         if user.access_profile == "student":
-            return render(request, 'office/student/index.html', context={"user": user})
+            return render(request, 'office/student/index.html', context=self.get_context_by_profile(user))
         elif user.access_profile == "teacher":
+            return render(request, 'office/teacher/index.html', context=self.get_context_by_profile(user))
 
-            teacher = Teacher.objects.get(profile__user=request.user)
-
-            return render(request, 'office/teacher/index.html', context={
+    def get_context_by_profile(self, user):
+        if user.access_profile == "student":
+            # context for student
+            return {"user": user}
+        elif user.access_profile == "teacher":
+            # context for teacher
+            teacher = Teacher.objects.get(profile__user=user.user)
+            return {
                 "user": user,
                 "subjects": teacher.subjects,
                 "files": teacher.files.all().order_by('subject__group__number', 'subject__name')
-            })
-
-
-        return render(request, 'office/not_access.html', context={})
+            }
--- subject/views.py
+++ subject/views.py
@@ -2,29 +2,32 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from office.models import *
+from office.models import Subject, Teacher, Files
 
 
 class SubjectIndex(LoginRequiredMixin, View):
+    """ Class for get html template of all subjects"""
     login_url = '/auth/login/'
     redirect_field_name = ''
 
     def get(self, request):
-        subjects = get_list_or_404(Subject)
+        subjects = get_list_or_404(Subject.objects.order_by('group__number', 'name'))
         return render(request, 'subject/index.html', context={
             "subjects": subjects,
         })
 
 
 class SubjectDetail(LoginRequiredMixin, View):
+    """ Class for get detail subject"""
     login_url = '/auth/login/'
     redirect_field_name = ''
 
     def get(self, request, slug):
 
-        subject = Subject.objects.get(slug=slug)
-        teacher = Teacher.objects.get(subjects__slug=slug)
-        files = Files.objects.all().filter(subject__slug=slug)
+        subject = get_object_or_404(Subject, slug=slug)
+        teacher = get_object_or_404(Teacher, subjects__slug=slug)
+        files = get_list_or_404(Files.objects.filter(subject__slug=slug))
+
         return render(request, 'subject/detail.html', context={
             "subject": subject,
             "teacher": teacher,
